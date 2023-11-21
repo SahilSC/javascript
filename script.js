997d3433ab88b1259309abcedf24683aff4ecf02
Sahil <sahilschowdhury@gmail.com>
2023-11-21 03:25:01 -0600

    alertconfirmprompt js

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("I am going to guess your birth year.");
+let name = prompt("What is your name?");
+if (name != null) {
+    let age = prompt("Input your age");
+    if (age != undefined) {
+        confirm(`You were born in ${2023-age}.`);
+    }
+    else {
+        alert("You need to type in a valid age!");
+    }
+}
+else {
+    alert("Please type in a valid name.")
+}
